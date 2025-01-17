ad75e83f838a1b9b1991495d475954f5e47da3f8
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-01-17 01:10:42 -0700

    Commit 4

--- ct1/main.cpp
+++ ct1/main.cpp
@@ -4,29 +4,33 @@
  *
  *  Created by Kaley Schlimgen on 1/17/25.
  *
- * Programming 3: CT 1
- * Console Application & Syntax Corrections
+ *  Programming 3: CT 1
+ *  Console Application
  *
- * To Do: Create simple C++ console application that will display:
- *   fictional person's: first & last name, address, city, zipcode
+ *  To Do: Create simple C++ console application that will display:
+ *         fictional person's: first & last name, address, city, zipcode
 */
 
 #include <iostream>
+#include<stdio.h>
 
-int main(int argc, const char * argv[]) {
+//So don't have to do "std::" before string and cout
+using namespace std;
+
+int main() {
     //Assigns variables with a string
-    std::string firstName = "Joe";
-    std::string lastName = "Smith";
-    std::string streetAddress = "123 S. St";
-    std::string city = "Littleton";
-    std::string zipCode = "80127";
+    string firstName = "Joe";
+    string lastName = "Smith";
+    string streetAddress = "123 S. St";
+    string city = "Littleton";
+    string zipCode = "80127";
     
     //Displays the variables and their assignments
-    std::cout << "First Name: " << firstName << std::endl;
-    std::cout << "Last Name: " << lastName << std::endl;
-    std::cout << "Street Address: " << streetAddress << std::endl;
-    std::cout << "City: " << city << std::endl;
-    std::cout << "Zip Code: " << zipCode << std::endl;
+    cout << "First Name: " << firstName << std::endl;
+    cout << "Last Name: " << lastName << std::endl;
+    cout << "Street Address: " << streetAddress << std::endl;
+    cout << "City: " << city << std::endl;
+    cout << "Zip Code: " << zipCode << std::endl;
     
     //return 0 because type is int (string is a class)
     return 0;
