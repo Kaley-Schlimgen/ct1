23ceca3ee867944834847c59aa7afefb152548b0
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-01-17 00:39:19 -0700

    Commit 2

--- ct1/main.cpp
+++ ct1/main.cpp
@@ -9,6 +9,17 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::string firstName = "Joe";
+    std::string lastName = "Smith";
+    std::string streetAddress = "123 S St";
+    std::string city = "Littleton";
+    std::string zipCode = "80127";
+    
+    std::cout << "First Name: " << firstName << std::endl;
+    std::cout << "Last Name: " << lastName << std::endl;
+    std::cout << "Street Address: " << streetAddress << std::endl;
+    std::cout << "City: " << city << std::endl;
+    std::cout << "Zip Code: " << zipCode << std::endl;
+    
     return 0;
 }
